3e7891660d97d5685e1d68bcff9a80060184636a
Victor Costa <victorcosta37@gmail.com>
2024-03-18 15:19:06 -0300

    chore: add urban request

--- api/urban/index.go
+++ api/urban/index.go
@@ -1,10 +1,50 @@
 package urban
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
+	"time"
 )
 
-func Handler(writer http.ResponseWriter, _ *http.Request) {
+type UrbanDictRes struct {
+	List []struct {
+		Definition  string    `json:"definition"`
+		Permalink   string    `json:"permalink"`
+		ThumbsUp    int       `json:"thumbs_up"`
+		Author      string    `json:"author"`
+		Word        string    `json:"word"`
+		Defid       int       `json:"defid"`
+		CurrentVote string    `json:"current_vote"`
+		WrittenOn   time.Time `json:"written_on"`
+		Example     string    `json:"example"`
+		ThumbsDown  int       `json:"thumbs_down"`
+	} `json:"list"`
+}
+
+func Handler(writer http.ResponseWriter, request *http.Request) {
+	term := request.URL.Query().Get("term")
+
+	var res *http.Response
+	if term == "" {
+		res, _ = http.Get("https://api.urbandictionary.com/v0/random")
+	} else {
+		res, _ = http.Get("https://api.urbandictionary.com/v0/define?term=" + term)
+	}
+
+	if res.StatusCode != 200 {
+		writer.WriteHeader(res.StatusCode)
+
+		log.Println("Error calling urban api", res.StatusCode)
+		return
+	}
+
+	var urbanDictRes UrbanDictRes
+
+	json.NewDecoder(res.Body).Decode(&urbanDictRes)
+
+	log.Println(urbanDictRes)
+
 	writer.WriteHeader(200)
 	writer.Write([]byte("it works!"))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module urban-dict
 
-go 1.21rc3
+go 1.21
